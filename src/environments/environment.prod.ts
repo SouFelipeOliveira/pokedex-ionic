035fe48e9da130a743f398483a463a3891c7339c
Felipe Oliveira <flipeaz342@gmail.com>
2024-03-10 11:52:03 -0400

    add en variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
+  pokeApi: 'https://pokeapi.co/api/v2',
+  imageURL: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon',
   production: true
 };
