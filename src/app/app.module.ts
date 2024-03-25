902ed457f90d06087300c36082d5a85f55484693
Felipe Oliveira <flipeaz342@gmail.com>
2024-03-24 21:38:48 -0400

    add index  to default-layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,21 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+
+import {
+  DefaultHeaderComponent,
+  DefaultLayoutComponent,
+  DefaultFooterComponent
+} from './containers';
+
+const CONTAINERS = [
+  DefaultHeaderComponent,
+  DefaultLayoutComponent,
+  DefaultFooterComponent
+];
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ...CONTAINERS],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
